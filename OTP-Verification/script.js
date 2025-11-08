114119b07be3204842f0e80838d7bf43149b5163
Aziz Cheniti <azizchniti0@gmail.com>
2025-11-08 17:45:38 +0100

    init commit

---
+++ OTP-Verification/script.js
@@ -0,0 +1,36 @@
+    const inputs = document.querySelectorAll(".otp-input");
+
+    inputs.forEach((input, index) => {
+      input.addEventListener("input", (e) => {
+        const val = e.target.value.replace(/[^0-9]/g, "");
+        e.target.value = val;
+
+        if (val) {
+          input.classList.add("filled");
+          if (index < inputs.length - 1) inputs[index + 1].focus();
+        } else {
+          input.classList.remove("filled");
+        }
+      });
+
+      input.addEventListener("keydown", (e) => {
+        if (e.key === "Backspace" && !input.value && index > 0) {
+          inputs[index - 1].focus();
+        }
+      });
+
+      input.addEventListener("paste", (e) => {
+        let paste = e.clipboardData.getData("text").trim();
+        paste = paste.replace(/[^0-9]/g, "");
+        if (paste.length) {
+          paste.split("").forEach((char, i) => {
+            if (inputs[i]) {
+              inputs[i].value = char;
+              inputs[i].classList.add("filled");
+            }
+          });
+          inputs[Math.min(paste.length - 1, inputs.length - 1)].focus();
+        }
+        e.preventDefault();
+      });
+    });
